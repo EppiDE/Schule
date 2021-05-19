40b395edfe0fb04cf91765f69988e70ed6b5831b
EppiDE <69919898+EppiDE@users.noreply.github.com>
2021-05-19 18:28:51 +0200

    Add files via upload

---
+++ Autoverwaltung/Autoverwaltung/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Autoverwaltung
+{
+    partial class FormVerwaltung
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormVerwaltung));
+            this.textBoxMarke = new System.Windows.Forms.TextBox();
+            this.textBoxModell = new System.Windows.Forms.TextBox();
+            this.textBoxFarbe = new System.Windows.Forms.TextBox();
+            this.textBoxBaujahr = new System.Windows.Forms.TextBox();
+            this.textBoxSpitzname = new System.Windows.Forms.TextBox();
+            this.textBoxKaufpreis = new System.Windows.Forms.TextBox();
+            this.labelMarke = new System.Windows.Forms.Label();
+            this.labelModell = new System.Windows.Forms.Label();
+            this.labelFarbe = new System.Windows.Forms.Label();
+            this.labelBaujahr = new System.Windows.Forms.Label();
+            this.labelSpitzname = new System.Windows.Forms.Label();
+            this.labelKaufpreis = new System.Windows.Forms.Label();
+            this.listBoxGarageAutos = new System.Windows.Forms.ListBox();
+            this.buttonNeuAnlegen = new System.Windows.Forms.Button();
+            this.buttonAktualisieren = new System.Windows.Forms.Button();
+            this.buttonLoeschen = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.labelAutoliste = new System.Windows.Forms.Label();
+            this.labelEingabe = new System.Windows.Forms.Label();
+            this.panelWelcomeScreen = new System.Windows.Forms.Panel();
+            this.buttonOkay = new System.Windows.Forms.Button();
+            this.labelWelcomeScreen = new System.Windows.Forms.Label();
+            this.buttonClearTextfields = new System.Windows.Forms.Button();
+            this.panelWelcomeScreen.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxMarke
+            // 
+            this.textBoxMarke.Location = new System.Drawing.Point(624, 127);
+            this.textBoxMarke.Name = "textBoxMarke";
+            this.textBoxMarke.Size = new System.Drawing.Size(242, 22);
+            this.textBoxMarke.TabIndex = 0;
+            // 
+            // textBoxModell
+            // 
+            this.textBoxModell.Location = new System.Drawing.Point(624, 181);
+            this.textBoxModell.Name = "textBoxModell";
+            this.textBoxModell.Size = new System.Drawing.Size(242, 22);
+            this.textBoxModell.TabIndex = 1;
+            // 
+            // textBoxFarbe
+            // 
+            this.textBoxFarbe.Location = new System.Drawing.Point(624, 237);
+            this.textBoxFarbe.Name = "textBoxFarbe";
+            this.textBoxFarbe.Size = new System.Drawing.Size(242, 22);
+            this.textBoxFarbe.TabIndex = 2;
+            // 
+            // textBoxBaujahr
+            // 
+            this.textBoxBaujahr.Location = new System.Drawing.Point(625, 291);
+            this.textBoxBaujahr.Name = "textBoxBaujahr";
+            this.textBoxBaujahr.Size = new System.Drawing.Size(242, 22);
+            this.textBoxBaujahr.TabIndex = 3;
+            // 
+            // textBoxSpitzname
+            // 
+            this.textBoxSpitzname.Location = new System.Drawing.Point(625, 347);
+            this.textBoxSpitzname.Name = "textBoxSpitzname";
+            this.textBoxSpitzname.Size = new System.Drawing.Size(242, 22);
+            this.textBoxSpitzname.TabIndex = 4;
+            // 
+            // textBoxKaufpreis
+            // 
+            this.textBoxKaufpreis.Location = new System.Drawing.Point(624, 403);
+            this.textBoxKaufpreis.Name = "textBoxKaufpreis";
+            this.textBoxKaufpreis.Size = new System.Drawing.Size(242, 22);
+            this.textBoxKaufpreis.TabIndex = 5;
+            // 
+            // labelMarke
+            // 
+            this.labelMarke.AutoSize = true;
+            this.labelMarke.Location = new System.Drawing.Point(622, 107);
+            this.labelMarke.Name = "labelMarke";
+            this.labelMarke.Size = new System.Drawing.Size(51, 17);
+            this.labelMarke.TabIndex = 6;
+            this.labelMarke.Text = "Marke:";
+            // 
+            // labelModell
+            // 
+            this.labelModell.AutoSize = true;
+            this.labelModell.Location = new System.Drawing.Point(622, 161);
+            this.labelModell.Name = "labelModell";
+            this.labelModell.Size = new System.Drawing.Size(53, 17);
+            this.labelModell.TabIndex = 7;
+            this.labelModell.Text = "Modell:";
+            // 
+            // labelFarbe
+            // 
+            this.labelFarbe.AutoSize = true;
+            this.labelFarbe.Location = new System.Drawing.Point(621, 217);
+            this.labelFarbe.Name = "labelFarbe";
+            this.labelFarbe.Size = new System.Drawing.Size(49, 17);
+            this.labelFarbe.TabIndex = 8;
+            this.labelFarbe.Text = "Farbe:";
+            // 
+            // labelBaujahr
+            // 
+            this.labelBaujahr.AutoSize = true;
+            this.labelBaujahr.Location = new System.Drawing.Point(622, 271);
+            this.labelBaujahr.Name = "labelBaujahr";
+            this.labelBaujahr.Size = new System.Drawing.Size(61, 17);
+            this.labelBaujahr.TabIndex = 9;
+            this.labelBaujahr.Text = "Baujahr:";
+            // 
+            // labelSpitzname
+            // 
+            this.labelSpitzname.AutoSize = true;
+            this.labelSpitzname.Location = new System.Drawing.Point(622, 327);
+            this.labelSpitzname.Name = "labelSpitzname";
+            this.labelSpitzname.Size = new System.Drawing.Size(78, 17);
+            this.labelSpitzname.TabIndex = 10;
+            this.labelSpitzname.Text = "Spitzname:";
+            // 
+            // labelKaufpreis
+            // 
+            this.labelKaufpreis.AutoSize = true;
+            this.labelKaufpreis.Location = new System.Drawing.Point(622, 383);
+            this.labelKaufpreis.Name = "labelKaufpreis";
+            this.labelKaufpreis.Size = new System.Drawing.Size(72, 17);
+            this.labelKaufpreis.TabIndex = 11;
+            this.labelKaufpreis.Text = "Kaufpreis:";
+            // 
+            // listBoxGarageAutos
+            // 
+            this.listBoxGarageAutos.FormattingEnabled = true;
+            this.listBoxGarageAutos.ItemHeight = 16;
+            this.listBoxGarageAutos.Location = new System.Drawing.Point(13, 98);
+            this.listBoxGarageAutos.MultiColumn = true;
+            this.listBoxGarageAutos.Name = "listBoxGarageAutos";
+            this.listBoxGarageAutos.Size = new System.Drawing.Size(300, 372);
+            this.listBoxGarageAutos.TabIndex = 12;
+            this.listBoxGarageAutos.SelectedIndexChanged += new System.EventHandler(this.listGarageAutos_SelectedIndexChanged);
+            // 
+            // buttonNeuAnlegen
+            // 
+            this.buttonNeuAnlegen.Location = new System.Drawing.Point(421, 181);
+            this.buttonNeuAnlegen.Name = "buttonNeuAnlegen";
+            this.buttonNeuAnlegen.Size = new System.Drawing.Size(118, 41);
+            this.buttonNeuAnlegen.TabIndex = 14;
+            this.buttonNeuAnlegen.Text = "Anlegen";
+            this.buttonNeuAnlegen.UseVisualStyleBackColor = true;
+            this.buttonNeuAnlegen.Click += new System.EventHandler(this.buttonNeuAnlegen_Click);
+            // 
+            // buttonAktualisieren
+            // 
+            this.buttonAktualisieren.Enabled = false;
+            this.buttonAktualisieren.Location = new System.Drawing.Point(421, 258);
+            this.buttonAktualisieren.Name = "buttonAktualisieren";
+            this.buttonAktualisieren.Size = new System.Drawing.Size(118, 42);
+            this.buttonAktualisieren.TabIndex = 15;
+            this.buttonAktualisieren.Text = "Aktualisieren";
+            this.buttonAktualisieren.UseVisualStyleBackColor = true;
+            this.buttonAktualisieren.Click += new System.EventHandler(this.buttonAktualisieren_Click);
+            // 
+            // buttonLoeschen
+            // 
+            this.buttonLoeschen.Enabled = false;
+            this.buttonLoeschen.Location = new System.Drawing.Point(441, 362);
+            this.buttonLoeschen.Name = "buttonLoeschen";
+            this.buttonLoeschen.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoeschen.TabIndex = 16;
+            this.buttonLoeschen.Text = "Löschen";
+            this.buttonLoeschen.UseVisualStyleBackColor = true;
+            this.buttonLoeschen.Click += new System.EventHandler(this.buttonLoeschen_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(1001, 456);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(137, 51);
+            this.buttonExit.TabIndex = 17;
+            this.buttonExit.Text = "Beenden";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // labelAutoliste
+            // 
+            this.labelAutoliste.AutoSize = true;
+            this.labelAutoliste.Location = new System.Drawing.Point(13, 75);
+            this.labelAutoliste.Name = "labelAutoliste";
+            this.labelAutoliste.Size = new System.Drawing.Size(56, 17);
+            this.labelAutoliste.TabIndex = 18;
+            this.labelAutoliste.Text = "Garage";
+            // 
+            // labelEingabe
+            // 
+            this.labelEingabe.AutoSize = true;
+            this.labelEingabe.Location = new System.Drawing.Point(700, 75);
+            this.labelEingabe.Name = "labelEingabe";
+            this.labelEingabe.Size = new System.Drawing.Size(64, 17);
+            this.labelEingabe.TabIndex = 20;
+            this.labelEingabe.Text = "Eingabe:";
+            // 
+            // panelWelcomeScreen
+            // 
+            this.panelWelcomeScreen.Controls.Add(this.buttonOkay);
+            this.panelWelcomeScreen.Controls.Add(this.labelWelcomeScreen);
+            this.panelWelcomeScreen.Location = new System.Drawing.Point(1144, 2);
+            this.panelWelcomeScreen.Name = "panelWelcomeScreen";
+            this.panelWelcomeScreen.Size = new System.Drawing.Size(1172, 505);
+            this.panelWelcomeScreen.TabIndex = 27;
+            // 
+            // buttonOkay
+            // 
+            this.buttonOkay.Location = new System.Drawing.Point(556, 420);
+            this.buttonOkay.Name = "buttonOkay";
+            this.buttonOkay.Size = new System.Drawing.Size(75, 37);
+            this.buttonOkay.TabIndex = 1;
+            this.buttonOkay.Text = "Okay";
+            this.buttonOkay.UseVisualStyleBackColor = true;
+            this.buttonOkay.Click += new System.EventHandler(this.buttonOkay_Click);
+            // 
+            // labelWelcomeScreen
+            // 
+            this.labelWelcomeScreen.AutoSize = true;
+            this.labelWelcomeScreen.Location = new System.Drawing.Point(273, 150);
+            this.labelWelcomeScreen.Name = "labelWelcomeScreen";
+            this.labelWelcomeScreen.Size = new System.Drawing.Size(610, 187);
+            this.labelWelcomeScreen.TabIndex = 0;
+            this.labelWelcomeScreen.Text = resources.GetString("labelWelcomeScreen.Text");
+            this.labelWelcomeScreen.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buttonClearTextfields
+            // 
+            this.buttonClearTextfields.Location = new System.Drawing.Point(671, 456);
+            this.buttonClearTextfields.Name = "buttonClearTextfields";
+            this.buttonClearTextfields.Size = new System.Drawing.Size(143, 29);
+            this.buttonClearTextfields.TabIndex = 28;
+            this.buttonClearTextfields.Text = "Felder leeren";
+            this.buttonClearTextfields.UseVisualStyleBackColor = true;
+            this.buttonClearTextfields.Click += new System.EventHandler(this.buttonClearTextfields_Click);
+            // 
+            // FormVerwaltung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1175, 519);
+            this.Controls.Add(this.buttonClearTextfields);
+            this.Controls.Add(this.panelWelcomeScreen);
+            this.Controls.Add(this.labelEingabe);
+            this.Controls.Add(this.labelAutoliste);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonLoeschen);
+            this.Controls.Add(this.buttonAktualisieren);
+            this.Controls.Add(this.buttonNeuAnlegen);
+            this.Controls.Add(this.listBoxGarageAutos);
+            this.Controls.Add(this.labelKaufpreis);
+            this.Controls.Add(this.labelSpitzname);
+            this.Controls.Add(this.labelBaujahr);
+            this.Controls.Add(this.labelFarbe);
+            this.Controls.Add(this.labelModell);
+            this.Controls.Add(this.labelMarke);
+            this.Controls.Add(this.textBoxKaufpreis);
+            this.Controls.Add(this.textBoxSpitzname);
+            this.Controls.Add(this.textBoxBaujahr);
+            this.Controls.Add(this.textBoxFarbe);
+            this.Controls.Add(this.textBoxModell);
+            this.Controls.Add(this.textBoxMarke);
+            this.Name = "FormVerwaltung";
+            this.Text = "Autoverwaltung";
+            this.panelWelcomeScreen.ResumeLayout(false);
+            this.panelWelcomeScreen.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxMarke;
+        private System.Windows.Forms.TextBox textBoxModell;
+        private System.Windows.Forms.TextBox textBoxFarbe;
+        private System.Windows.Forms.TextBox textBoxBaujahr;
+        private System.Windows.Forms.TextBox textBoxSpitzname;
+        private System.Windows.Forms.TextBox textBoxKaufpreis;
+        private System.Windows.Forms.Label labelMarke;
+        private System.Windows.Forms.Label labelModell;
+        private System.Windows.Forms.Label labelFarbe;
+        private System.Windows.Forms.Label labelBaujahr;
+        private System.Windows.Forms.Label labelSpitzname;
+        private System.Windows.Forms.Label labelKaufpreis;
+        private System.Windows.Forms.ListBox listBoxGarageAutos;
+        private System.Windows.Forms.Button buttonNeuAnlegen;
+        private System.Windows.Forms.Button buttonAktualisieren;
+        private System.Windows.Forms.Button buttonLoeschen;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelAutoliste;
+        private System.Windows.Forms.Label labelEingabe;
+        private System.Windows.Forms.Panel panelWelcomeScreen;
+        private System.Windows.Forms.Label labelWelcomeScreen;
+        private System.Windows.Forms.Button buttonOkay;
+        private System.Windows.Forms.Button buttonClearTextfields;
+    }
+}
+
---
+++ Autoverwaltung/Autoverwaltung/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//@@@@@@@ Autor: Marc Eppinger
+//@@@@@@@ Programname: Âutoverwaltung
+//@@@@@@@ Beschreibung:
+//@@@@@@@ – Einfaches Tool zum anlegen einer Autodatenbank (ohne Speicher)
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+namespace Autoverwaltung
+{
+    public partial class FormVerwaltung : Form
+    {
+        public FormVerwaltung()
+        {
+            InitializeComponent();
+        }
+
+        public struct strucAuto
+        {
+            public string Marke;
+            public string Modell;
+            public string Farbe;
+            public int Baujahr;
+            public string Spitzname;
+            public double Kaufpreis;
+            /*public override string ToString()
+            {
+                return Spitzname;
+            }*/
+            
+        }
+        
+
+        List<strucAuto> listAuto = new List<strucAuto>();
+
+        private void buttonNeuAnlegen_Click(object sender, EventArgs e)
+        {
+            addListToListbox(addItemToList());
+        }
+
+        private strucAuto addItemToList()
+        {
+            strucAuto scLocalAuto = new strucAuto();
+            int intParsedValue;
+            double dbParsedValue;
+            scLocalAuto.Marke = textBoxMarke.Text;
+            scLocalAuto.Modell = textBoxModell.Text;
+            scLocalAuto.Farbe = textBoxFarbe.Text;
+            if (textBoxSpitzname.Text.Length > 0)
+            {
+                scLocalAuto.Spitzname = textBoxSpitzname.Text;
+            }
+            else
+            {
+                scLocalAuto.Spitzname = "404";
+            }
+
+            if (int.TryParse(textBoxBaujahr.Text, out intParsedValue))
+            {
+                scLocalAuto.Baujahr = Convert.ToInt32(textBoxBaujahr.Text);
+            }
+            else if (textBoxBaujahr == null || !int.TryParse(textBoxBaujahr.Text, out intParsedValue))
+            {
+                scLocalAuto.Baujahr = 0;
+            }
+            if (textBoxBaujahr.Text.Length > 0 && !int.TryParse(textBoxBaujahr.Text, out intParsedValue))
+            {
+                MessageBox.Show("Ungültige Jahreszahl!\r\nBitte Aktualisieren!");
+            }
+            if (double.TryParse(textBoxKaufpreis.Text, out dbParsedValue))
+            {
+                scLocalAuto.Kaufpreis = Convert.ToDouble(textBoxKaufpreis.Text);
+            }
+            else if (textBoxKaufpreis == null || !double.TryParse(textBoxKaufpreis.Text, out dbParsedValue))
+            {
+                scLocalAuto.Kaufpreis = 0;
+            }
+            if (textBoxKaufpreis.Text.Length > 0 && !double.TryParse(textBoxKaufpreis.Text, out dbParsedValue))
+            {
+                MessageBox.Show("Ungültiger Kaufpreis!\r\nBitte Aktualisieren!");
+            }
+            return scLocalAuto;
+        }
+        private void addListToListbox(strucAuto lscAuto)
+        {
+            listBoxGarageAutos.Items.Clear();
+            listAuto.Add(lscAuto);
+            foreach (strucAuto localstrucAuto in listAuto)
+            {
+                listBoxGarageAutos.Items.Add(localstrucAuto);
+            }
+        }
+
+        private void buttonAktualisieren_Click(object sender, EventArgs e)
+        {
+            int localSelected = listBoxGarageAutos.SelectedIndex;
+            listBoxGarageAutos.Items.RemoveAt(localSelected);
+            listBoxGarageAutos.Items.Insert(localSelected, addItemToList());
+        }
+
+        private void buttonLoeschen_Click(object sender, EventArgs e)
+        {
+            //int localSelected = listBoxGarageAutos.SelectedIndex;
+            //listBoxGarageAutos.Items.RemoveAt(localSelected);
+            strucAuto localStrucAutoSelected = new strucAuto();
+            localStrucAutoSelected = (strucAuto)listBoxGarageAutos.SelectedItem;
+            listBoxGarageAutos.Items.Remove(localStrucAutoSelected);
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void showSelectedItem()
+        {
+            strucAuto localScAuto = (strucAuto)listBoxGarageAutos.SelectedItem;
+            textBoxModell.Text = localScAuto.Modell;
+            textBoxBaujahr.Text = Convert.ToString(localScAuto.Baujahr);
+            textBoxFarbe.Text = localScAuto.Farbe;
+            textBoxKaufpreis.Text = Convert.ToString(localScAuto.Kaufpreis);
+            textBoxMarke.Text = localScAuto.Marke;
+            textBoxKaufpreis.Text = localScAuto.Spitzname;
+        }
+        private void clearTextboxes()
+        {
+            textBoxBaujahr.ResetText();
+            textBoxFarbe.ResetText();
+            textBoxKaufpreis.ResetText();
+            textBoxMarke.ResetText();
+            textBoxModell.ResetText();
+            textBoxSpitzname.ResetText();
+        }
+        private void listGarageAutos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxGarageAutos.SelectedIndex < 0)
+            {
+                buttonAktualisieren.Enabled = false;
+                buttonLoeschen.Enabled = false;
+                //clearTextboxes();
+            }
+            else
+            {
+                buttonAktualisieren.Enabled = true;
+                buttonLoeschen.Enabled = true;
+                showSelectedItem();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOkay_Click(object sender, EventArgs e)
+        {
+            panelWelcomeScreen.Enabled = false;
+            panelWelcomeScreen.Visible = false;
+        }
+
+        private void saveStreamAsFile(string filePath, Stream inputStream, string fileName)
+        {
+            DirectoryInfo info = new DirectoryInfo(filePath);
+            if (!info.Exists)
+            {
+                info.Create();
+            }
+
+            string path = Path.Combine(filePath, fileName);
+            using (FileStream outputFileStream = new FileStream(path, FileMode.Create))
+            {
+                inputStream.CopyTo(outputFileStream);
+            }
+        }
+
+        private void buttonClearTextfields_Click(object sender, EventArgs e)
+        {
+            clearTextboxes();
+        }
+    }
+}
